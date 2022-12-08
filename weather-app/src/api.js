a9cb981fecdb515f453221829a5e985ba9be4cb5
TruongGiaTien <truongtien577@gmail.com>
2022-12-09 01:40:25 +0700

    Lấy dữ liệu thời tiết từ trang openweathermap

--- weather-app/src/api.js
+++ weather-app/src/api.js
@@ -8,7 +8,5 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
-fetch('/cities', geoApiOptions)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "a3cccb9be90561aa4ff429fc27b15078";
