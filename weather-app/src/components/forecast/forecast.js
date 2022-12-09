4a54e7f0054708581e4299ecf4f2ae1f01f91136
TruongGiaTien <truongtien577@gmail.com>
2022-12-09 14:06:35 +0700

    cấu trúc file forecast.js lấy dữ liệu 7 ngày tiếp theo trong tuần để dự báo

--- weather-app/src/components/forecast/forecast.js
+++ weather-app/src/components/forecast/forecast.js
@@ -1,20 +1,33 @@
 import { Accordion, AccordionItem, AccordionItemButton, AccordionItemHeading, AccordionItemPanel } from "react-accessible-accordion";
 
+const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+
 const Forecast = ({data}) => {
+    const dayInAWeek = new Date().getDay();
+    const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek));
+
+    console.log(forecastDays)
     return (
         <>
             <label className="title">Daily</label>
             <Accordion allowZeroExpanded>
-                {/* {data.list.slice(0, 7).map((item, idx) => (
+                {data.list.slice(0, 7).map((item, idx) => (
                     <AccordionItem key={idx}>
                             <AccordionItemHeading>
                                 <AccordionItemButton>
-                                    Hello
+                                    <div className="daily-item">
+                                        <img alt="weather" className="icon-small" src={`icons/${item.weather[0].icon}.png`}></img>
+                                        <label className="day">{forecastDays[idx]}</label>
+                                        <label className="description">{item.weather[0].description}</label>
+                                        <label className="min-max">{Math.round(item.main.temp_min)}℃ / {Math.round(item.main.temp_max)}℃</label>
+                                        
+                                    </div>
                                 </AccordionItemButton>
                             </AccordionItemHeading>
                             <AccordionItemPanel></AccordionItemPanel>
                     </AccordionItem>
-                ))} */}
+                ))}
             </Accordion>
         </>
     );
